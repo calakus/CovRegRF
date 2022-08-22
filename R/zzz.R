9a43680a6cc98653b94287027ebe625f14d0e7ed
cansu alakus <cansu.alakus@hec.ca>
2022-08-22 10:48:00 -0400

    vignette is updated

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-  corerf.version <- read.dcf(file=system.file("DESCRIPTION", package=pkgname),
-                             fields="Version")
-  packageStartupMessage(paste("\n",
-                              pkgname,
-                              corerf.version))
-}
